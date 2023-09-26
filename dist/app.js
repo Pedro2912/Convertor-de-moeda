6eba6f1d5e9a5bff4bf8818b0cabde68fb264581
Pedro123 <ph4055586@gmail.com>
2023-09-26 15:29:42 -0300

    list

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,28 @@
+async function data(){
+let dd = await fetch("data.json");
+let datd = await dd.json()
+ datd = JSON.stringify(datd)
+datd = JSON.parse(datd)
+return datd 
+}
+let oo43 
+async function ddd () {
+let o =  await data()
+let o1 = o.currency
+ oo43 =  o1
+}
+ddd()
 const country1 = document.getElementById("coutry")
 const recomend = document.querySelector('.recomended')
 const ul = recomend.children[0]
-const recomendedCoutries = []
 const ll = document.createElement('li')
 country1.addEventListener('input',()=>{
+	console.log(oo43)
 if (country1.value === '') {
 recomend.classList.add('hidden')	
 }else{
-recomend.classList.remove('hidden')
-ul.innerHTML = ll.textContent='dede'
- }
-})
+recomend.classList.remove('hidden')	
+};
+
+});
 
