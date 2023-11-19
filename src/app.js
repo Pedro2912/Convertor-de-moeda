6de72235c851cd88880a6bdf184f72193520870e
Pedro123 <ph4055586@gmail.com>
2023-11-19 11:49:06 -0300

    f

--- src/app.js
+++ src/app.js
@@ -4,16 +4,15 @@ document.addEventListener(("click"),()=>{
 	recomend.classList.add("hidden")
 	recomendRes.classList.add('hidden')
 })
-let oo43
 let code
 async function data(){
 let dd = await fetch("dist/data.json");
 let datd = await dd.json()
 datd = JSON.parse(datd)
-oo43 = datd.currency
 code = datd.abreviation
+return datd.currency
 }
-data()
+let oo43 =  data() 
 const country1 = document.getElementById("coutry")
 const countryRes = document.getElementById("coutryResult")
 
